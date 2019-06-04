145da9801c7009270f1dee7baa678154c1cd3e75
Alina <vorchilova@inbox.ru>
2019-06-04 22:32:17 +0300

    first commit slider

---
+++ element-search/slider/task.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const getSliderItemActive = document.getElementsByClassName("slider__item");
+const arrowPrev = document.getElementsByClassName("slider__arrow slider__arrow_prev");
+const arrowNext = document.getElementsByClassName("slider__arrow slider__arrow_next");
+let numberSlide = 0;
+let prevSlide;
+
+arrowPrev.item(0).onclick = function() {
+    prevSlide = numberSlide;
+    numberSlide = numberSlide - 1;
+    if (numberSlide === -1) { numberSlide = 4;}
+    getSliderItemActive.item(numberSlide).className = "slider__item slider__item_active";
+    getSliderItemActive.item(prevSlide).className = "slider__item";
+    return numberSlide;
+}
+
+arrowNext.item(0).onclick = function() {
+   prevSlide = numberSlide;
+   numberSlide = numberSlide + 1;
+   if (numberSlide === 5) { numberSlide = 0;}
+   console.log(numberSlide);
+   getSliderItemActive.item(numberSlide).className = "slider__item slider__item_active";
+   getSliderItemActive.item(prevSlide).className = "slider__item";
+   return numberSlide;
+}
