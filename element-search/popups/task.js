b4eb6f936039d87f510174b50e19fca4dd4cd80a
Alina <vorchilova@inbox.ru>
2019-06-06 22:51:59 +0300

    corrected popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,21 +1,20 @@
 "use strict";
-document.getElementById("modal_main").className = "modal modal_active";
-
+const modalMain = document.getElementById("modal_main");
+const modalSuccess = document.getElementById("modal_success");
 const arrModalClose = document.getElementsByClassName("modal__close");
 const arrModalSuccess = document.getElementsByClassName("show-success");
-
 const arrModalCloseLength = arrModalClose.length;
 
-for (var i=0; i < arrModalCloseLength; i++) {
+modalMain.classList.add("modal_active");
+
+for (var i = 0; i < arrModalCloseLength; i++) {
   arrModalClose.item(i).onclick = function() {
-  document.getElementById("modal_main").className = "modal";
+  modalMain.classList.remove("modal_active");
+  modalSuccess.classList.remove("modal_active");
   }
 }
 
-
-
 arrModalSuccess.item(0).onclick = function() {
-  document.getElementById("modal_success").className = "modal modal_active";
+  modalMain.classList.remove("modal_active");
+  modalSuccess.classList.add("modal_active");
 }
-
-
