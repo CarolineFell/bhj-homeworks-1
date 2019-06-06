3c25e0c5fdd6f9ce847c523f3b48f28ae208dccd
Alina <vorchilova@inbox.ru>
2019-06-06 22:59:13 +0300

    corrected menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,6 +5,14 @@ let findMenuSubElements = document.getElementsByClassName("menu_sub");
 const lengthMenuSubElements = findMenuSubElements.length;
   for (let i = 0; i < lengthMenuSubElements; i++) {
   findMenuSubElements.item(i).previousElementSibling.onclick = function() {
-     findMenuSubElements.item(i).className = "menu menu_sub menu_active";
+     event.preventDefault();
+     findMenuSubElements.item(i).classList.add("menu_active");
+      for (let j = 0; j < lengthMenuSubElements; j++) {
+        if (j != i ) {
+          findMenuSubElements.item(j).classList.remove("menu_active");
+        }
   }
-}
+    }
+  }
+
+
